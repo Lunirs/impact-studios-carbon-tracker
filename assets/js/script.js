21ae4c25899d78f6696e69b37e0c674e85ddb733
sdmnny <sdmnny@gmail.com>
2022-05-23 13:20:39 -0400

    added inital modal with script

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+// modal function 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  var instance = M.Modal.getInstance(elem);
+
+  instance.open();
+  instance.close();
