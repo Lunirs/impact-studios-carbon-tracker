d82c81adc35093082483e2aef9a1cbe2df8064b0
elliotzacharywaxman <elliot@climatedao.xyz>
2022-05-24 13:52:08 -0400

    commiting the new name for js about

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,26 +1,3 @@
-var requestOptions = {
-  method: "GET",
-};
-
-var params = {
-  api_token: "TNPak6m6ig7UHDYZQqczuFZHKtzZBIGaIgx8DZ8q",
-  categories: "general,tech",
-  search: "climate change",
-  limit: "50",
-};
-
-var esc = encodeURIComponent;
-var query = Object.keys(params)
-  .map(function (k) {
-    return esc(k) + "=" + esc(params[k]);
-  })
-  .join("&");
-
-fetch("https://api.thenewsapi.com/v1/news/all?" + query, requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
-
 // modal initialization
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".modal");
